653dccd0f6dd8d5e6776273f1b76340d2fe28506
Clint Williams <158440016+williamsclintwayne@users.noreply.github.com>
2024-10-31 21:38:38 +0200

    finish node api version 2

--- server.js
+++ server.js
@@ -1,99 +1,52 @@
+require("dotenv").config();
 const express = require(`express`);
-const mongoose = require('mongoose');
-const Product = require('./models/productModel');
+const mongoose = require("mongoose");
+const productRoute = require(`./routes/productRoute`);
+const errorMiddleware = require(`./middleware/errorMiddleware`);
+var cors = require("cors");
+
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+const MONGO_URL = process.env.MONGO_URL;
+const FRONTEND = process.env.FRONTEND;
+
+// this is used to only give specific origin access to the api data
+var corsOptions = {
+  origin: FRONTEND,
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+// use this to add cors policy
+app.use(cors(corsOptions));
 // use this so that the app can understand to use .json types
 app.use(express.json());
 // use this so that the app can understand to use forms types
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 // routes
 
-app.get('/', (req, res) => {
-    res.send(`hello NODE API`);
+app.use("/api/product", productRoute);
+
+app.get("/", (req, res) => {
+  res.send(`hello NODE API`);
 });
 
-app.get('/blog', (req, res) => {
-    res.send(`hello blog My name is Clint`);
+app.get("/blog", (req, res) => {
+  res.send(`hello blog My name is Clint`);
 });
 
-// get all products
-app.get('/product', async(req,res) => {
-    try {
-        const products = await Product.find({});
-        res.status(200).json(products);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({message: error.message});
-    }
-});
-
-// get a product by id
-app.get('/product/:id', async(req,res) => {
-    try {
-        const {id} = req.params;
-        const product = await Product.findById(id);
-        res.status(200).json(product);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({message: error.message});
-    }
-});
-
-// create a product
-app.post('/product', async(req, res) => {
-    try {
-        const product = await Product.create(req.body);
-        res.status(200).json(product);
-
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({message: error.message});
-    }
-});
-
-// update a product
-app.put('/product/:id', async(req,res) => {
-    try {
-        const {id} = req.params;
-        const product = await Product.findByIdAndUpdate(id, req.body);
-        // we cannot find any product in database
-        if(!product){
-            return res.status(404).json({message: `cannot find any product with ID ${id}`});
-        }
-        // const updatedProduct = await Product.findById(id);
-        res.status(200).json(product);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({message: error.message});
-    }
-});
-
-// delete a product
-app.delete('/product/:id', async(req,res) => {
-    try {
-        const {id} = req.params;
-        const product = await Product.findByIdAndDelete(id);
-        // we cannot find any product in database
-        if(!product){
-            return res.status(404).json({message: `cannot find any product with ID ${id}`});
-        }
-        res.status(200).json(product);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({message: error.message});
-    }
-})
+app.use(errorMiddleware);
 
 mongoose.set("strictQuery", false);
-mongoose.
-connect('mongodb+srv://williamsclintwayne:Cw36690000@clintapi.fh6lm.mongodb.net/Node-API?retryWrites=true&w=majority&appName=ClintAPI')
-.then(() => {
-    console.log('connected to MongodB');
-    app.listen(3000, ()=> {
-        console.log(`Node API app is running on port 3000`)
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    console.log("connected to MongodB");
+    app.listen(PORT, () => {
+      console.log(`Node API app is running on port ${PORT}`);
     });
-}).catch(() => {
+  })
+  .catch((error) => {
     console.log(error);
-})
+  });
